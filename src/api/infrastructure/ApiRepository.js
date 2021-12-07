2e2053e3419716bacc66f0830d9aa3d42b7bfa98
UxerDevCoding <omarheviaarbana@gmail.com>
2021-12-07 12:37:05 +0100

    CardSection file added and ApiCall created

---
+++ src/api/infrastructure/ApiRepository.js
@@ -0,0 +1,12 @@
+export default class ApiRepository {
+  constructor(url) {
+    this.url = url;
+  }
+
+  async getAllData() {
+    const response = await fetch(this.url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+}
---
+++ src/components/CardSection.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  name: "CardSection",
+  props: {
+    animalList: Object,
+  },
+  data: () => ({}),
+};
+</script>
